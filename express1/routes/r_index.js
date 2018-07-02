13412466ee29e1c194dc564594347cfdbd919b75
liufeifeng <liufeifeng@jtoushou.com>
2018-07-02 11:40:21 +0800

    add axios

--- express1/routes/r_index.js
+++ express1/routes/r_index.js
@@ -2,32 +2,40 @@ var express = require('express');
 var router = express.Router();
 // var ApiMethod = require('../ApiMethod/ApiMethod');
 var co = require('co');
+var axios = require('axios');
+// import axios from 'axios'
 router.get('/index.html',function(req,res){
-    var pageData = {
-        title:'lyx的express',
-        show:{
-            name:'lyxx',
-            tel:'135678905'
-        },
-        // tel:'444345'
-    };
-    var data={};
+    // var pageData = {
+    //     title:'lyx的express',
+    //     show:{
+    //         name:'lyxx',
+    //         tel:'135678905'
+    //     },
+    //     // tel:'444345'
+    // };
+    // var data={};
     co(function *(){
-<<<<<<< HEAD
-        // data = yield ApiMethod.post('https://alpha-agw.jintoushou.com/gateway/problem/categories',{});
-=======
-        data = yield ApiMethod.post('https://alpha-agw.jintoushou.com/gateway/problem/categoriesss',{});
->>>>>>> a57a86e3b263de3d93d50d5fc82a4bbc4f72fc93
-        console.log('lyxdata',data);
-        if(data.data && data.data.length){
-            pageData.data = data.data;
-        }else{
-            if (data.errCode === 400 || data.errCode === '400'){
-                console.log('格式错误',data.errCode)
-            }
-            pageData.data = [];
-        }
-        res.render('../views/index',pageData);
+
+    //     data = yield ApiMethod.post('https://alpha-agw.jintoushou.com/gateway/problem/categoriesss',{});
+    //     console.log('lyxdata',data);
+    //     if(data.data && data.data.length){
+    //         pageData.data = data.data;
+    //     }else{
+    //         if (data.errCode === 400 || data.errCode === '400'){
+    //             console.log('格式错误',data.errCode)
+    //         }
+    //         pageData.data = [];
+    //     }
+        axios.post('http://192.168.130.103:88/API/getdata.json')//URL已增加时间戳
+        .then(function (response) {
+        console.log('success',response.status,response.data)
+        
+        })
+        .catch(function (error) {
+        console.error('console-error.config', error);
+
+        });
+        res.render('../views/index');
     
     });
 });
